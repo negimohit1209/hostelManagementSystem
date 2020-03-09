c85006ad99ceae10bc880074119b9e26fe2b1caa
negimohit1209 <mohitsngh1997@gmail.com>
2020-03-09 23:55:41 +0530

    clearing up the mess I have done in 4th year

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Hostel Management System
+
 This is a web app that will help students apply for the hostel of their choice and also help the warden collect fees. More features will be added soon.
 
 ###### The project is still under construction.
 
 ## Built With
 
-* [Node.js](https://nodejs.org/en/) - Backend technology
-* [Express.js](https://expressjs.com/) - Creating APIs
-* [MongoDB](https://www.mongodb.com/) - Database
-
-
+- [Node.js](https://nodejs.org/en/) - Backend technology
+- [Express.js](https://expressjs.com/) - Creating APIs
+- [MongoDB](https://www.mongodb.com/) - Database
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 var bodyParser = require("body-parser");
 var passport = require("passport");
 var methodOverride = require("method-override");
@@ -8,38 +8,38 @@ var moment = require("moment");
 
 const app = express();
 const port = process.env.PORT || 3000;
-var {mongoose} = require('./config/mongoose');
-var User = require('./models/user');
-
+var { mongoose } = require("./config/mongoose");
+var User = require("./models/user");
 
 // ROUTES DECLARATION
-const indexRoutes = require('./routes/index');
-const dashboardRoutes =  require('./routes/dashboard');
-const applicationRoutes =  require('./routes/application');
-const billRoutes = require('./routes/bill');
+const indexRoutes = require("./routes/index");
+const dashboardRoutes = require("./routes/dashboard");
+const applicationRoutes = require("./routes/application");
+const billRoutes = require("./routes/bill");
 
 // additional configuration
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/assets"));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(flash());
 
-
 //configuring passport
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "this will be used for the hash function",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    app.locals.moment = require('moment');
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  app.locals.moment = require("moment");
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use(passport.initialize());
@@ -53,9 +53,8 @@ app.use("/dashboard", dashboardRoutes);
 app.use("/dashboard/application", applicationRoutes);
 app.use("/dashboard/bill", billRoutes);
 
-
-app.get("/",(req,res)=>{
-    res.render("landing");
+app.get("/", (req, res) => {
+  res.render("landing");
 });
 
-app.listen(port, ()=> console.log(`app is running on port ${port}`));
+app.listen(port, () => console.log(`app is running on port ${port}`));
